1a99899d6d508d2af13088f76084c4d35dfeb09c
daviaquino87 <davi26031@gmail.com>
2022-08-10 22:40:31 -0300

    rebase(business rule):business rule applied

--- src/index.js
+++ src/index.js
@@ -35,6 +35,9 @@ app.get('/statement/:cpf',(request,response) => {
 
     const customer = customers.find((customer) => customer.cpf === cpf);
 
+    if(!customer){
+        return response.status(400).json({error:"costumer not found!"})
+    }
     return response.json(customer.statement);
 })
 
